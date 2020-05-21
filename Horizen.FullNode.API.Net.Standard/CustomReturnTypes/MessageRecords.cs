85d254836043b127ca8fa76858378a90c3a624b5
PeaStew <peter.gavin.stewart@gmail.com>
2020-05-21 17:36:38 +0200

    Added some processing classes for blockchain messages using OP_RETURN

---
+++ Horizen.FullNode.API.Net.Standard/CustomReturnTypes/MessageRecords.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Horizen.FullNode.API.Net.Standard.CustomReturnTypes
+{
+    public class MessageRecords
+    {
+        public MessageRecords()
+        {
+            Messages = new List<MessageRecord>();
+        }
+
+        public int LastBlockId { get; set; }
+        public string LastSearchedAt { get; set; }
+
+        public List<MessageRecord> Messages { get; set; }
+    }
+}
